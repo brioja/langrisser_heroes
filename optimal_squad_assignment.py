6894a6f547a7e96a5801ff9ffb30b775ab8d3854
Brandon Rioja <34580058+brioja@users.noreply.github.com>
2023-12-27 10:58:36 -0500

    feat: add parameter for number of squads

--- optimal_squad_assignment.py
+++ optimal_squad_assignment.py
@@ -12,26 +12,26 @@ def get_all_factions(heroes_data):
         factions.update(hero_factions)
     return list(factions)
 
-def calculate_heroes_in_squads(squads, heroes_data):
-    heroes_in_squads = set()
+def calculate_total_heroes(squads, heroes_data):
+    total_heroes = 0
     for squad in squads:
         for hero, hero_factions in heroes_data.items():
             if any(faction in squad for faction in hero_factions):
-                heroes_in_squads.add(hero)
-    return len(heroes_in_squads)
+                total_heroes += 1
+    return total_heroes
 
-def find_optimal_squad_assignments(heroes_data):
+def find_optimal_squad_assignments(heroes_data, num_squads):
     factions = get_all_factions(heroes_data)
     max_heroes = 0
     best_assignment = None
 
-    total_combinations = sum(1 for _ in itertools.combinations(factions, 8))
+    total_combinations = sum(1 for _ in itertools.combinations(factions, num_squads * 2))
     processed_combinations = 0
 
-    for faction_combination in itertools.combinations(factions, 8):
-        for squads in itertools.permutations(faction_combination, 8):
-            squads = [squads[i:i+2] for i in range(0, 8, 2)]
-            num_heroes = calculate_heroes_in_squads(squads, heroes_data)
+    for faction_combination in itertools.combinations(factions, num_squads * 2):
+        for squads in itertools.permutations(faction_combination, num_squads * 2):
+            squads = [squads[i:i+2] for i in range(0, num_squads * 2, 2)]
+            num_heroes = calculate_total_heroes(squads, heroes_data)
             if num_heroes > max_heroes:
                 max_heroes = num_heroes
                 best_assignment = squads
@@ -43,16 +43,19 @@ def find_optimal_squad_assignments(heroes_data):
     return best_assignment, max_heroes
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <path_to_json_file>")
+    if len(sys.argv) < 3:
+        print("Usage: python script.py <path_to_json_file> <number_of_squads>")
         sys.exit(1)
 
     file_path = sys.argv[1]
+    num_squads = int(sys.argv[2])
+
     heroes_data = read_json(file_path)
-    optimal_assignment, max_heroes = find_optimal_squad_assignments(heroes_data)
+    optimal_assignment, max_heroes = find_optimal_squad_assignments(heroes_data, num_squads)
 
     print("\nOptimal Squad Assignments:", optimal_assignment)
-    print("Maximum number of heroes covered:", max_heroes)
+    print("Total number of heroes (including duplicates):", max_heroes)
 
 if __name__ == "__main__":
     main()
+
